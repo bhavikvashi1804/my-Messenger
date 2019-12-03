e85348d91e3bc408f131a0a6843b2c0687dc9f1a
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2019-12-03 12:55:42 +0530

    Animation 007 Animated Text Kit

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -2,6 +2,9 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 
+
+import 'package:animated_text_kit/animated_text_kit.dart';
+
 class WelcomeScreen extends StatefulWidget {
   static const String id="welcome_screen";
   @override
@@ -60,9 +63,9 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
                     height: 60,
                   ),
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
+                TypewriterAnimatedTextKit(
+                  text: ['Flash Chat'],
+                  textStyle: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
                   ),
