1f9330e8cb21889cdb57db3e7d54629cef0c62e8
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2019-12-03 11:54:01 +0530

    Custom Animation 1

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -8,11 +8,29 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
+
+  AnimationController controller;
+
+  @override
+  void initState() {
+    controller=AnimationController(duration: Duration(seconds: 1),vsync:this, );
+    controller.forward();
+    controller.addListener((){
+        setState(() {
+          
+        });
+        //print(controller.value);
+        //prints value of animation star from 0-1
+      }
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.red.withOpacity(controller.value),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
