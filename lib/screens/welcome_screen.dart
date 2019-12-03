b2aa8044e878228ddbd7ed8528213253b986e528
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2019-12-03 13:55:26 +0530

    Refactoring

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -5,6 +5,9 @@ import 'package:flutter/material.dart';
 
 import 'package:animated_text_kit/animated_text_kit.dart';
 
+
+import '../widgets/button_widget.dart';
+
 class WelcomeScreen extends StatefulWidget {
   static const String id="welcome_screen";
   @override
@@ -25,13 +28,9 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
       end: Colors.white,
     ).animate(controller);
     controller.forward();
-    
     controller.addListener((){
         setState(() {
-          
         });
-        //print(animation.value);
-        //prints value of animation star from 0-1
       }
     );
     super.initState();
@@ -75,44 +74,8 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to login screen.
-                    Navigator.pushNamed(context, LoginScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to registration screen.
-                    Navigator.pushNamed(context, RegistrationScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
-            ),
+            myButton(text: 'Log In',c: Colors.lightBlueAccent, onPress: (){ Navigator.pushNamed(context, LoginScreen.id);},),
+            myButton(text: 'Register',c: Colors.blueAccent, onPress: (){ Navigator.pushNamed(context, RegistrationScreen.id);},),
           ],
         ),
       ),
---
+++ lib/widgets/button_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class myButton extends StatelessWidget {
+
+  Function onPress;
+  Color c;
+  String text;
+
+  myButton({this.text,this.c,this.onPress});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: c,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: onPress,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            text,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
